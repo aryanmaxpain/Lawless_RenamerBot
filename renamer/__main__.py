2a0671ba65dd24113327cb83d31954119244b6ae
Lawless_Shikari <91068760+ShikariBaaZ@users.noreply.github.com>
2022-05-05 20:18:45 +0530

    Create __main__.py

---
+++ renamer/__main__.py
@@ -0,0 +1,45 @@
+import logging
+
+# Get logging configurations
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - Line: %(lineno)d - Path: %(name)s - Module: %(module)s.py - %(levelname)s - %(message)s',
+                    datefmt='%d/%m/%Y %I:%M:%S %p')
+logging.getLogger().setLevel(logging.DEBUG)
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger().setLevel(logging.ERROR)
+logging.getLogger().setLevel(logging.WARNING)
+
+import platform
+from .config import Config
+from pyrogram import Client, __version__, idle
+from pyromod import listen
+
+
+def main():
+
+    Renamer = Client("Lawless_RenamerBot",
+                 bot_token=Config.BOT_TOKEN,
+                 api_id=Config.API_ID,
+                 api_hash=Config.API_HASH,
+                 plugins=dict(root="renamer/plugins"),
+                 workers=100)
+
+    Renamer.start()
+    me = Renamer.get_me()
+
+    startup_msg = f"Successfully deployed your lawless Renamer Bot at @{me.username}\n"
+    startup_msg += f"Pyrogram Version: V{__version__}\n"
+    startup_msg += f"Python Version: V{platform.python_version()}\n\n"
+    startup_msg += "Special Thanks to you for Deploying our Lawless Renamer Bot. If you like this Repo then Dont FORGET to Give a star to our Repo and join here for Latest Updates @Lawless_Updates."
+    print(startup_msg)
+
+    idle()
+
+    Renamer.stop()
+    print("Ok bye , Be Happy 😢.")
+
+if __name__ == "__main__":
+    main()
+
+
+
