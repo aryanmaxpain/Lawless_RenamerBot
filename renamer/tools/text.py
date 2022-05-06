e2c89711e6c76e5b64cd41f9aa9386fa162ecdc8
Lawless_Shikari <91068760+ShikariBaaZ@users.noreply.github.com>
2022-05-06 11:35:35 +0530

    Create text.py

---
+++ renamer/tools/text.py
@@ -0,0 +1,63 @@
+from pyrogram.emoji import *
+
+class TEXT:
+    DOWNLOAD_START = f"Downloading Starts soon {SLEEPING_FACE}"
+    UPLOAD_START = f"Upload Starting Soon {SLEEPING_FACE}"
+    UPLOAD_SUCESS = f"Thanks for using [ShikariBaaZ](https://github.com/ShikariBaaZ/Lawless_RenamerBot)"
+    BANNED_USER_TEXT = f"Hey Dear, you are **banned** from using me {FACE_WITH_TEARS_OF_JOY}."
+    NOT_LOGGED_TEXT = f"This bot was made only for private use {LOCKED_WITH_KEY}. If you want to use this bot you need to send me correct password in the format `/login bot_password`"
+    SAVED_CUSTOM_THUMBNAIL = f"Thumbnail Saved Permanently {NOTEBOOK_WITH_DECORATIVE_COVER}"
+    DELETED_CUSTOM_THUMBNAIL = f"Thumbnail Deleted Successfully {CHECK_MARK_BUTTON}"
+    NO_CUSTOM_THUMB_NAIL_FOUND = f"𝖭𝗈 𝗍𝗁𝗎𝗆𝖻𝗇𝖺𝗂𝗅 𝖥𝗈𝗎𝗇𝖽 {THUMBS_DOWN_LIGHT_SKIN_TONE}"
+    THUMBNAIL_CAPTION = f"{BACKHAND_INDEX_POINTING_UP_LIGHT_SKIN_TONE} Your Permanent thumbnail"
+
+
+    ABOUT = """**𝖬𝗒 𝖣𝖾𝗍𝖺𝗂𝗅𝗌 :**
+
+** My Name:** {bot_name}
+    
+** Language:** [Python 3](https://www.python.org/)
+
+** FrameWork:** [Pyrogram](https://github.com/pyrogram/pyrogram)
+
+** Developer:** {bot_owner}
+
+** Channel:** [💐Lawless UPDATES💐](https://t.me/Lawless_Updates)
+
+** Group:** [💐Lawless Fellas Hub💐](https://t.me/Lawless_Fellas_Hub)
+
+** Source Code:** [🍁Press Me🍁](https://github.com/ShikariBaaZ/Lawless_RenamerBot)
+"""
+
+    HELP_USER = """**Follow Below Steps:**
+   
+☞︎︎︎ Use /mode command to change upload mode.
+☞︎︎︎ Send a photo to set as permanent thumbnail.
+☞︎︎︎ Now send me the Telegram file you want to rename.
+☞︎︎︎ Send the new name when bot ask.
+
+For source code check about
+"""
+
+    START_TEXT = """Hi {user_mention},
+
+Hey Dear🌺, I am a Telegram Lawless renamer Bot with permanent thumbnail Support.
+
+𝖥𝗈𝗋 𝖬𝗈𝗋𝖾 𝖣𝖾𝗍𝖺𝗂𝗅𝗌 Check out 𝖧𝖾𝗅𝗉.
+
+**Maintained By:** {bot_owner}
+"""
+
+
+    DONATE_USER = """**__❤️Thanks for showing your interest in donation & giving your precious time❤️.__**
+ 
+❤️Your Donation will help us to keep our Services alive & Permanent❤️
+❤️You can Donate, Whatever you want to.. 
+❤️There is no limit for your Donation..
+❤️Thanks❤️
+ 
+__--Payment Methods:--__
+ 
+GooglePay / Paytm / UPI
+ 
+**For Donate:** Message Me Here @The_Shikarii"""
